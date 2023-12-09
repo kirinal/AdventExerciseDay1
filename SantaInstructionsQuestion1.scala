8c6d96cc2b08e473179536bc1ecad4731c8a9128
kirinal <114201036+kirinal@users.noreply.github.com>
2023-12-09 11:22:58 -0600

    Add files via upload

---
+++ SantaInstructionsQuestion1.scala
@@ -0,0 +1,27 @@
+object SantaInstructionsQuestion1 {
+
+        def findFloor(instructions: String): Int = {
+        var floor = 0
+
+        for ((char, position) <- instructions.zipWithIndex) {
+        if (char == '(') {
+        floor += 1
+        } else if (char == ')') {
+        floor -= 1
+        }
+
+        if (floor == -1) {
+        return position + 1
+        }
+        }
+
+        -1
+        }
+
+        def main(args: Array[String]): Unit = {
+        println("Enter the instructions:")
+        val instructions = scala.io.StdIn.readLine()
+        val finalFloor = findFloor(instructions)
+        println(s"To what floor do the instructions take Santa? Santa ends up on floor $finalFloor.")
+        }
+        }
